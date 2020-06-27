c599f73b730e700867ba98f512ef78476f9bd41d
kongming <liangkongming@gmail.com>
2020-06-27 17:05:24 +0800

    fix_chinese_char

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 ### Install
 * Download [Anaconda Python3](https://www.anaconda.com/products/individual) based on your system.
+* Git clone this project.
+* Build package: pyinstaller -F main.py
+* If you encounter maximum recursion depth problem:
+  * try to add the following codes into main.spec and rebuild the package with 'pyinstaller main.spec'
+    ```python
+    import sys
+    sys.setrecursionlimit(5000)
+    ```
+  * Actually, I use the [solution](https://stackoverflow.com/a/60756018/4168774) which can solve this problem elegantly.
+* If you encounter no module named 'pkg_resources.py2_warn', try to 
+    ```
+    pip install --upgrade 'setuptools<45.0.0'
+    ```
+    
+### Usage
+* put all the xlsx into input_files directory
+* output_files contains the processed xlsx and log files
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+import os
 import os.path as osp
+import sys
 from datetime import datetime
 from glob import glob
 import openpyxl
-from openpyxl.styles import PatternFill, GradientFill
+from openpyxl.styles import PatternFill
+import warnings
+
+warnings.filterwarnings("ignore")
 
 def load_file_to_list(filename):
     out_list = []
-    with open(filename) as f:
+    with open(filename, 'rb') as f:
         for line in f.readlines():
             out_list.append(line.strip())
     return out_list
 
 def find_key_word(cell, key_words):
     for key_word in key_words:
-        if isinstance(cell.value, str) and key_word in cell.value:
+        # from IPython import embed; embed()
+        if isinstance(cell.value, str) and key_word.decode("utf-8") in cell.value:
             return key_word
     return None
 
@@ -24,7 +30,7 @@ def process_sheet(sheet, key_words_dict):
                 continue
             key_word = find_key_word(cell, key_words)
             if key_word is not None:
-                key_words_dict[key_word].append('{}'.format(cell))
+                key_words_dict[key_word].append('{}'.format(cell.coordinate))
                 cell.value = ''
                 cell.fill = PatternFill("solid", fgColor="000000")
 
@@ -39,19 +45,37 @@ def run_redaction(file_name, key_words, logger):
     wb.save(osp.join('output_files', osp.basename(file_name)))
 
     for key_word in key_words:
-        logger.write('========= {} =========\n'.format(key_word))
+        logger.write('========= {} =========\n'.format(key_word.decode("utf-8")).encode("utf-8"))
         for sheet_name, key_words_dict in sheet_dict.items():
             key_words_result = key_words_dict[key_word]
             if len(key_words_result) > 0:
-                logger.write('{}, {}'.format(sheet_name, key_words_result))
+                logger.write('{}: {}\n'.format(sheet_name, ','.join(key_words_result)).encode("utf-8"))
+                # logger.write('{}, {}'.format(sheet_name, key_words_result))
+                # print('{}, {}'.format(sheet_name, key_words_result))
 
 if __name__ == '__main__':
-    current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    logger = open('output_files/log-{}.txt'.format(current_time), 'w')
+    # check input xlsx files and keyword txt
     input_files = glob('input_files/*.xlsx')
+    if len(input_files) == 0:
+        print('No xlsx files are found in input_files directory.')
+        input("Press Enter to Quit...")
+        sys.exit(0)
+
+    if not osp.exists('input_files/keywords.txt'):
+        print('Keywords.txt can not be found in input_files directory.')
+        input("Press Enter to Quit...")
+        sys.exit(0)
     key_words = load_file_to_list('input_files/keywords.txt')
+
+    # check output directory
+    if not osp.exists('output_files'):
+        os.mkdir('output_files')
+    current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    logger = open('output_files/log-{}.txt'.format(current_time), 'wb')
+    print('Hi, I am processing your files one by one...')
     for input_file in input_files:
         print(input_file)
         run_redaction(input_file, key_words, logger)
-        break
     logger.close()
+    print('All files have been processed.')
+    input("Press Enter to Quit...")
